1e6d35e281f39e18296994186af840cdaaa98cda
Daniel7R <dr734659@gmail.com>
2022-03-04 16:21:34 -0500

    emm

--- src/components/RegisterForm/index.js
+++ src/components/RegisterForm/index.js
@@ -9,11 +9,6 @@ export const RegisterForm= () => {
                 <Input type="text" placeholder="Digite su apellido" id="last-name" name="last-name" />
                 <Input type="email" placeholder="Digite su correo" id="email" name="email" />
                 <Input type="password" placeholder="Digite su contraseña" id="password" name="password" />
-                <Input type="password" placeholder="Digite nuevamente su contraseña" id="re-password" name="re-password" />
-                <Select>
-                    <option value="renter">Rentador</option>
-                    <option value="searcher">Buscador</option>
-                </Select>
             </Div>
             <Button type="button">Enviar</Button>
         </Form>
