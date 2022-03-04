0f3063be19fd2a748b43bdf9c6897a6fd459323a
Daniel7R <dr734659@gmail.com>
2022-03-04 16:26:53 -0500

    f

--- src/components/RegisterForm/index.js
+++ src/components/RegisterForm/index.js
@@ -3,15 +3,18 @@ import { Form, Input, Div, Button, Select } from "./styles";
 
 export const RegisterForm= () => {
     return (
-        <Form>
-            <Div>
-                <Input type="text" placeholder="Digite su nombre" id="name" name="name" />
-                <Input type="text" placeholder="Digite su apellido" id="last-name" name="last-name" />
-                <Input type="email" placeholder="Digite su correo" id="email" name="email" />
-                <Input type="password" placeholder="Digite su contraseña" id="password" name="password" />
-            </Div>
-            <Button type="button">Enviar</Button>
-        </Form>
+        <>
+            <Form>
+                <Div>
+                    <Input type="text" placeholder="Digite su nombre" id="name" name="name" />
+                    <Input type="text" placeholder="Digite su apellido" id="last-name" name="last-name" />
+                    <Input type="email" placeholder="Digite su correo" id="email" name="email" />
+                    <Input type="password" placeholder="Digite su contraseña" id="password" name="password" />
+                </Div>
+                <Button type="button">Enviar</Button>
+            </Form>
+        </>
+        
     )
 }
 
