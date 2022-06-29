35a27098116906408f583f413b704d952e613b30
Carlos Daniel Rivera Rangel <58122746+Daniel7R@users.noreply.github.com>
2022-06-29 10:55:46 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 export const firebaseConfig = {
-  apiKey: process.env.API_KEY_FIREBASE,
-  authDomain: process.env.AUTH_DOMAIN_FIREBASE,
-  projectId: process.env.PROJECT_ID_FIREBASE,
-  storageBucket: process.env.STORAGE_BUCKET_FIREBASE,
-  messagingSenderId: process.env.MESSAGING_SENDERID_FIREBASE,
-  appId: process.env.APP_ID_FIREBASE
+  apiKey: process.env.REACT_APP_API_KEY_FIREBASE,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN_FIREBASE,
+  projectId: process.env.REACT_APP_PROJECT_ID_FIREBASE,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET_FIREBASE,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDERID_FIREBASE,
+  appId: process.env.REACT_APP_APP_ID_FIREBASE
 };
 
 const app= initializeApp(firebaseConfig)
